6434cbd6f68634109ec87972cf23dd9aec448e36
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-05 11:27:23 +0300

    feat: add polybar

---
+++ linux/polybar/launch.sh
@@ -0,0 +1,8 @@
+
+# polybar-msg cmd quit
+#
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+#
+# echo "Bars launched..."

587e586f7ebd184b969b2139748dc4a9f3a8454a
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-10 12:41:27 +0300

    feat: add wezterm

---
+++ linux/wezterm/.wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 14
+config.window_background_opacity = 0.88
+config.enable_tab_bar = false
+
+return config

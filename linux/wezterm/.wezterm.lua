3c8f7bb4e284d65eaf87e4ddfe68bcca234d8b56
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-21 16:24:58 +0300

    feat: some udpates

--- linux/wezterm/.wezterm.lua
+++ linux/wezterm/.wezterm.lua
@@ -2,10 +2,26 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 500 })
-config.font_size = 15
+config.font_size = 14
 config.window_background_opacity = 0.6
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
+
+config.keys = {
+	{
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			local sel = window:get_selection_text_for_pane(pane)
+			if not sel or sel == "" then
+				window:perform_action(wezterm.action.SendKey({ key = "c", mods = "CTRL" }), pane)
+			else
+				window:perform_action(wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }), pane)
+			end
+		end),
+	},
+	{ key = "v", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+}
 
 config.colors = {
 	cursor_bg = "white",

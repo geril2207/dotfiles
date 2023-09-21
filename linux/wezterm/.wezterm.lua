383f8bbd2418c1fd0dc39f39a5824f8a0c98ff95
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-21 14:04:15 +0300

    feat: update term configs

--- linux/wezterm/.wezterm.lua
+++ linux/wezterm/.wezterm.lua
@@ -1,9 +1,14 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 14
-config.window_background_opacity = 0.88
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 500 })
+config.font_size = 15
+config.window_background_opacity = 0.6
 config.enable_tab_bar = false
+config.window_decorations = "NONE"
+
+config.colors = {
+	cursor_bg = "white",
+}
 
 return config

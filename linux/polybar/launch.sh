c262e982e6216c5599653300d0d5423a705744bc
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-26 13:45:42 +0300

    feat(polybar): multi monitor

--- linux/polybar/launch.sh
+++ linux/polybar/launch.sh
@@ -8,4 +8,11 @@
 
 polybar-msg cmd quit
 
-polybar main
+# polybar main
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi

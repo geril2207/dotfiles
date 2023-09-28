a12f3be631efe551eca6e1d888049a59ad1957fd
Gergert Ilya <gergert.ilya@mail.ru>
2023-09-28 17:25:48 +0300

    feat(wezterm): update

--- linux/wezterm/.wezterm.lua
+++ linux/wezterm/.wezterm.lua
@@ -7,6 +7,12 @@ config.window_background_opacity = 0.75
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 config.keys = {
 	{
 		key = "c",

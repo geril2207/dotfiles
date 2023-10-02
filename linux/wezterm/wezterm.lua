e481c5882388707de72aa95b78e4efe1441df215
Gergert Ilya <gergert.ilya@mail.ru>
2023-10-02 10:59:42 +0300

    refactor(wezterm): config

--- linux/wezterm/wezterm.lua
+++ linux/wezterm/wezterm.lua
@@ -1,46 +1,47 @@
 local wezterm = require("wezterm")
-local config = {}
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 400 })
-config.font_size = 15.0
-config.window_background_opacity = 0.8
-config.bold_brightens_ansi_colors = false
-config.enable_tab_bar = false
-config.window_decorations = "NONE"
-config.color_scheme = "Tango (terminal.sexy)"
+return {
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = 300 }),
+	font_size = 15.0,
+	window_background_opacity = 0.8,
+	bold_brightens_ansi_colors = false,
+	enable_tab_bar = false,
+	window_decorations = "NONE",
+	color_scheme = "Tango (terminal.sexy)",
+	-- adjust_window_size_when_changing_font_size = false,
 
--- config.window_background_gradient = {
--- 	colors = {
--- 		"#181825",
--- 		-- "#1e2030",
--- 	},
--- }
-
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-config.keys = {
-	{
-		key = "c",
-		mods = "CTRL",
-		action = wezterm.action_callback(function(window, pane)
-			local sel = window:get_selection_text_for_pane(pane)
-			if not sel or sel == "" then
-				window:perform_action(wezterm.action.SendKey({ key = "c", mods = "CTRL" }), pane)
-			else
-				window:perform_action(wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }), pane)
-			end
-		end),
+	colors = {
+		cursor_bg = "white",
+		cursor_border = "white",
 	},
-	{ key = "v", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
-}
 
-config.colors = {
-	cursor_bg = "white",
-	cursor_border = "white",
-}
+	-- window_background_gradient = {
+	-- 	colors = {
+	-- 		"#181825",
+	-- 		-- "#1e2030",
+	-- 	},
+	-- },
 
-return config
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
+	keys = {
+		{
+			key = "c",
+			mods = "CTRL",
+			action = wezterm.action_callback(function(window, pane)
+				local sel = window:get_selection_text_for_pane(pane)
+				if not sel or sel == "" then
+					window:perform_action(wezterm.action.SendKey({ key = "c", mods = "CTRL" }), pane)
+				else
+					window:perform_action(wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }), pane)
+				end
+			end),
+		},
+		{ key = "v", mods = "CTRL", action = wezterm.action.PasteFrom("Clipboard") },
+	},
+}

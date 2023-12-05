051cf8982b3a05afa3cf191efc49af2ba1af5631
geril2207 <gergert.ilya@mail.ru>
2023-12-05 19:11:33 +0300

    update

--- linux/i3/workpc_startup.sh
+++
@@ -1,6 +0,0 @@
-xinput set-prop "ELAN072B:00 04F3:31BF Touchpad" "libinput Tapping Enabled" 1
-xinput set-prop "ELAN072B:00 04F3:31BF Touchpad" "libinput Natural Scrolling Enabled"  1
-xinput set-prop "ELAN072B:00 04F3:31BF Touchpad" "libinput Scrolling Pixel Distance" 30
-xinput set-prop "PixArt Microsoft USB Optical Mouse" "Coordinate Transformation Matrix" 0.7 0 0 0 0.7 0 0 0 1
-
-xrandr --output HDMI-A-0 --primary

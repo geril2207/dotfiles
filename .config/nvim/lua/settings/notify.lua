f1f7073ee50b756f7e4e00ba8d35d5204f372325
geril <gergert.ilya@mail.ru>
2025-02-22 12:14:31 +0300

    upd

--- .config/nvim/lua/settings/notify.lua
+++ .config/nvim/lua/settings/notify.lua
@@ -4,5 +4,8 @@ function vim.notify(msg, level)
 	if msg:find("WARNING: vim.treesitter.get_parser") or msg:find("position_encoding param is required") then
 		return
 	end
+	if msg:find("editor.action.triggerSuggest") then
+		return
+	end
 	old_notify(msg, level)
 end

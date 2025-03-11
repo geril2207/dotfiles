fdf91b9d0abf673faaea38753eec844bf7408100
geril <gergert.ilya@mail.ru>
2025-03-11 16:00:28 +0300

    rewrite crypt.mjs

--- scripts/crypt.mjs
+++ scripts/crypt.mjs
@@ -1,4 +1,6 @@
+#!/usr/bin/env node
 import crypto from "node:crypto";
+import { readFile, writeFile } from "node:fs/promises";
 
 const algorithm = "aes-256-ecb";
 
@@ -21,32 +23,51 @@ const ACTIONS = {
   DECRYPT: "decrypt",
 };
 
-const main = () => {
-  const [action, secret, input] = process.argv
-    .slice(2, 5)
+/**
+ * @param {string} outputFile
+ * @param {string} content
+ * */
+const writeOutput = (outputFile, content) => {
+  if (!content.endsWith("\n")) {
+    content += "\n";
+  }
+  return writeFile(outputFile, content, { encoding: "utf8" });
+};
+
+const main = async () => {
+  const [action, secretFile, inputFile, outputFile] = process.argv
+    .slice(2, 6)
     .map((item) => item.trim());
-  if (!Object.values(ACTIONS).includes(action)) {
-    throw new Error("Invalid action");
+
+  const validations = [
+    {
+      condition: !Object.values(ACTIONS).includes(action),
+      msg: "Invalid action",
+    },
+    { condition: !secretFile, msg: "Invalid secret file" },
+    { condition: !inputFile, msg: "Invalid input file" },
+    { condition: !outputFile, msg: "Invalid output file" },
+  ];
+
+  for (const validation of validations) {
+    if (validation.condition) throw new Error(validation.msg);
   }
-  if (!secret) {
-    throw new Error("Secret key didn't provided");
-  }
-  if (!input) {
-    throw new Error("No input");
-  }
+
+  const input = (await readFile(inputFile, { encoding: "utf8" })).trim();
+  const secret = (await readFile(secretFile, { encoding: "utf8" })).trim();
 
   const key = crypto.createHash("sha256").update(secret).digest();
 
   switch (action) {
     case ACTIONS.ENCRYPT: {
-      const encrypted = encrypt(input, key);
-      console.log(encrypted);
+      const result = encrypt(input, key);
+      await writeOutput(outputFile, result);
       return;
     }
 
     case ACTIONS.DECRYPT: {
-      const decrypted = decrypt(input, key);
-      console.log(decrypted);
+      const result = decrypt(input, key);
+      await writeOutput(outputFile, result);
       return;
     }
 

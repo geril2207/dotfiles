01a83b24d192ae83174be374159519b8e5aea01d
geril <gergert.ilya@mail.ru>
2025-03-14 20:20:07 +0300

    upd

--- bin/xinput-set.sh
+++ bin/xinput-set.sh
@@ -6,27 +6,25 @@ VAL=$3
 
 DEFAULT="Default"
 
-if [ "$DEVICE" = "" ]; then 
-    exit 1
+if [ "$DEVICE" = "" ]; then
+  exit 1
 fi
 
-if [ "$PROP" = "" ]; then 
-    exit 1
+if [ "$PROP" = "" ]; then
+  exit 1
 fi
 
-if [ "$VAL" = "" ]; then 
-    exit 1
+if [ "$VAL" = "" ]; then
+  exit 1
 fi
 
 devlist=$(xinput --list | grep "$DEVICE" | sed -n 's/.*id=\([0-9]\+\).*/\1/p')
 echo $devlist
 
-for dev in $devlist
-do
-    props=$(xinput list-props $dev | grep "$PROP" | grep -v $DEFAULT | sed -n 's/.*(\([0-9]\+\)).*/\1/p')
+for dev in $devlist; do
+  props=$(xinput list-props $dev | grep "$PROP" | grep -v $DEFAULT | sed -n 's/.*(\([0-9]\+\)).*/\1/p')
 
-    for prop in $props
-    do
-        xinput set-prop $dev $prop $VAL 
-    done 
+  for prop in $props; do
+    xinput set-prop $dev $prop $VAL
+  done
 done

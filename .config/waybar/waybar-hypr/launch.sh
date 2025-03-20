c0f3a45b74d4db75b1355cc3aebf25e638236dcc
geril <gergert.ilya@mail.ru>
2025-03-20 19:30:44 +0300

    upd

---
+++ .config/waybar/waybar-hypr/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+pkill waybar
+
+# Wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+DIR_PATH=~/.config/waybar/waybar-hypr
+CONFIG_PATH=$DIR_PATH/config.jsonc
+STYLE_PATH=$DIR_PATH/style.css
+
+# Launch main
+waybar -c $CONFIG_PATH -s $STYLE_PATH >/dev/null 2>&1 &
--- .config/waybar/waybar-sway/launch.sh
+++ .config/waybar/waybar-sway/launch.sh
@@ -6,5 +6,9 @@ pkill waybar
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
+DIR_PATH=~/.config/waybar/waybar-sway
+CONFIG_PATH=$DIR_PATH/config.jsonc
+STYLE_PATH=$DIR_PATH/style.css
+
 # Launch main
-waybar >/dev/null 2>&1 &
+waybar -c $CONFIG_PATH -s $STYLE_PATH >/dev/null 2>&1 &

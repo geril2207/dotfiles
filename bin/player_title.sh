80176b606239187d0503284ce08f14346f06eeae
geril <gergert.ilya@mail.ru>
2025-03-20 22:09:14 +0300

    upd

--- bin/player_title.sh
+++ bin/player_title.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
 output=$(playerctl metadata --format='{{ artist }} - {{ title }}')
+output=$(echo "$output" | xargs)
 
-# If no player found, exit silently
 if [[ "$output" == "No players found" ]]; then
   exit 0
 fi
 
-# Trim spaces
-output=$(echo "$output" | xargs)
-
-# Return if empty after trim
 if [[ -z "$output" ]]; then
   exit 0
 fi
 
-# If starts with " - ", strip it
-if [[ $output == " - "* ]]; then
-  cleaned="${output:3}"
+if [[ $output == "- "* ]]; then
+  cleaned="${output:2}"
 else
   cleaned="$output"
 fi

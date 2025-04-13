3cb80ee15dd27c7239676e92f3a4ee2d10d7f484
geril <gergert.ilya@mail.ru>
2025-04-13 13:53:13 +0300

    upd

---
+++ win/win-run-sing-box.ps1
@@ -0,0 +1,11 @@
+$instanceId = "SWD\Wintun\{143AEA73-ACA5-6C86-B5DD-16D301C1F64D}"
+
+Write-Host "Removing Wintun device: $instanceId"
+pnputil /remove-device "$instanceId"
+
+# Replace this with the command you want to run
+Write-Host "Running follow-up command..."
+
+Set-Location "$HOME/my/dotfiles/.config/sing-box"
+
+sing-box run -c .\ignored\1.json 2> asd.log

4ecf6d89654208b37b937a31a94c61e25e74be06
geril <gergert.ilya@mail.ru>
2025-05-12 12:38:40 +0300

    grug-far

---
+++ .config/nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,19 @@
+-- Find and replace.
+return {
+	{
+		"MagicDuck/grug-far.nvim",
+		opts = {},
+		cmd = "GrugFar",
+		-- keys = {
+		-- 	{
+		-- 		"<leader>cg",
+		-- 		function()
+		-- 			local grug = require("grug-far")
+		-- 			grug.open({ transient = true })
+		-- 		end,
+		-- 		desc = "GrugFar",
+		-- 		mode = { "n", "v" },
+		-- 	},
+		-- },
+	},
+}
